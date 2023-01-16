c727b67941a3c45cba16de3c29dde98282544893
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2023-01-16 23:53:21 +0200

    Multidimensional Arrays - Exercise
    Add problem 1 and 2

---
+++ C#Advanced - January 2023/Multidimensional Arrays - Exercise/1.DiagonalDifference/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace _1.DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int size = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[size, size];
+
+            for (int row = 0; row < size; row++)
+            {
+                int[] numbers = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = numbers[col];
+                }
+            }
+
+            int sumLeftDiagonal = 0;
+            int sumRightDiagonal = 0;
+
+            for (int row = 0; row < size; row++)
+            {
+                sumLeftDiagonal += matrix[row, row];
+            }
+
+            for (int row = size-1; row >= 0; row--)
+            {
+                sumRightDiagonal += matrix[row, size-1-row];
+
+            }
+
+            Console.WriteLine(Math.Abs(sumRightDiagonal-sumLeftDiagonal));
+
+        }
+    }
+}
---
+++ C#Advanced - January 2023/Multidimensional Arrays - Exercise/2.SquaresInMatrix/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace _2.SquaresInMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x))
+                .ToArray();
+
+            char[,] matrix = new char[size[0], size[1]];
+
+            for (int row = 0; row < size[0]; row++)
+            {
+                char[] charecters = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x =>char.Parse(x))
+                    .ToArray();
+
+                for (int col = 0; col < size[1]; col++)
+                {
+                    matrix[row, col] = charecters[col];
+                }
+            }
+
+            int counter = 0;
+
+            for (int row = 0; row < size[0]-1; row++)
+            {
+                for (int col = 0; col < size[1]-1; col++)
+                {
+                    if(matrix[row,col] == matrix[row, col +1] 
+                        && matrix[row, col] == matrix[row+1, col]
+                        && matrix[row, col] == matrix[row + 1, col + 1])
+                    {
+                        counter++;
+                    }
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
