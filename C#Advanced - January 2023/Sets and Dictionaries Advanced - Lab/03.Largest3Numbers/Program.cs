7f6582fe5b954ddb84f42762bfa9b072a3797131
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2023-01-20 23:16:19 +0200

    Sets and Dictionaries Advanced - Lab
    Add problems 3 and 4

---
+++ C#Advanced - January 2023/Sets and Dictionaries Advanced - Lab/03.Largest3Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.Largest3Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            numbers = numbers.OrderByDescending(n => n).ToList();
+
+            if (numbers.Count < 3)
+            {
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    Console.Write($"{numbers[i]} ");
+                }
+                
+            }
+            else
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write($"{numbers[i]} ");
+
+                }
+            }
+
+        }
+    }
+}
---
+++ C#Advanced - January 2023/Sets and Dictionaries Advanced - Lab/04.ProductShop/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.ProductShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, double>> shops = new Dictionary<string, Dictionary<string, double>>();
+
+            string input = Console.ReadLine();
+
+            while (input != "Revision")
+            {
+                string[] command = input.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                string nameShop = command[0];
+                string product = command[1];
+                double price = double.Parse(command[2]);
+
+                if (!shops.ContainsKey(nameShop))
+                {
+                    shops.Add(nameShop, new Dictionary<string, double>());
+                }
+
+                shops[nameShop].Add(product, price);
+
+                input = Console.ReadLine();
+            }
+
+            shops = shops.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var item in shops)
+            {
+                Console.WriteLine($"{item.Key}->");
+
+                foreach (var product in item.Value)
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+                }
+            }
+
+        }
+    }
+}
