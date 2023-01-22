f12b94a6c45ba1232463c302315e489278d73b0a
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2023-01-22 22:58:53 +0200

    Sets and Dictionaries Advanced - Lab
    Add problems 5 and 6

---
+++ C#Advanced - January 2023/Sets and Dictionaries Advanced - Lab/05.CitiesByContinentAndCountry/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.CitiesByContinentAndCountry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int times = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, List<string>>> continents = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < times; i++)
+            {
+                string[] data = Console.ReadLine().Split().ToArray();
+
+                string continent = data[0];
+                string countre = data[1];
+                string sity = data[2];
+
+                if (!continents.ContainsKey(continent))
+                {
+                    continents.Add(continent, new Dictionary<string, List<string>>());
+                }
+                if (!continents[continent].ContainsKey(countre))
+                {
+                    continents[continent].Add(countre, new List<string>());
+                }
+
+                continents[continent][countre].Add(sity);
+
+            }
+
+            foreach (var curenContinent in continents)
+            {
+                Console.WriteLine($"{curenContinent.Key}:");
+
+                foreach (var country in curenContinent.Value)
+                {
+                    Console.WriteLine($"{country.Key} -> {string.Join(", ", country.Value)}");
+                }
+            }
+        }
+    }
+}
