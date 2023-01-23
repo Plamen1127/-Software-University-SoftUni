82688ada2db8c2f75a42802562f2744870ec0620
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2023-01-23 22:27:44 +0200

    Sets and Dictionaries Advanced - Lab
    Add problems

---
+++ C#Advanced - January 2023/Sets and Dictionaries Advanced - Lab/07.ParkingLot/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.ParkingLot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> carsNumbers = new HashSet<string>();
+
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                string[] command = input.Split(", ");
+
+                if (command[0] == "IN")
+                {
+                    carsNumbers.Add(command[1]);
+                }
+                else
+                {
+                    carsNumbers.Remove(command[1]);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (carsNumbers.Count > 0)
+            {
+                foreach (var numberCar in carsNumbers)
+                {
+                    Console.WriteLine(numberCar);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
---
+++ C#Advanced - January 2023/Sets and Dictionaries Advanced - Lab/08.SoftUniParty/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.SoftUniParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> guests = new HashSet<string>();
+            List<string> regularGuests = new List<string>();
+
+            string reservation = Console.ReadLine();
+
+            while (reservation != "PARTY")
+            {
+                guests.Add(reservation);
+
+                reservation = Console.ReadLine();
+            }
+
+            while (reservation != "END")
+            {
+                guests.Remove(reservation);
+                reservation = Console.ReadLine();
+            }
+
+            Console.WriteLine(guests.Count);
+
+
+            foreach (var item in guests)
+            {
+                
+                if (char.IsDigit(item[0]))
+                {
+                    Console.WriteLine(item);
+                }
+                else
+                {
+                    regularGuests.Add(item);
+                }
+            }
+
+            foreach (var item in regularGuests)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
