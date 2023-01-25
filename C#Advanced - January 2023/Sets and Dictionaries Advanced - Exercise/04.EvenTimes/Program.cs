4e297dfc3ae841945b042638bf5c636da4b311ca
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2023-01-25 23:50:44 +0200

    Sets and Dictionaries Advanced - Exercise
    Add problems

---
+++ C#Advanced - January 2023/Sets and Dictionaries Advanced - Exercise/04.EvenTimes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.EvenTimes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                if (!numbers.ContainsKey(input))
+                {
+                    numbers.Add(input, 0);
+                }
+
+                numbers[input]++;
+            }
+
+            Console.WriteLine(numbers.Single(n => n.Value % 2 == 0).Key);
+        }
+    }
+}
