1d692292fe4f923e8f00c1015e5062b5a8576006
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2023-01-26 23:18:10 +0200

    Sets and Dictionaries Advanced - Exercise
    Add problems

---
+++ C#Advanced - January 2023/Sets and Dictionaries Advanced - Exercise/06.Wardrobe/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.Wardrobe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, int>> clothes = new Dictionary<string, Dictionary<string, int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] data = Console.ReadLine()
+                    .Split(new string[] { " -> ", "," }, StringSplitOptions.RemoveEmptyEntries);
+                string color = data[0];
+
+                if (!clothes.ContainsKey(color))
+                {
+                    clothes.Add(color, new Dictionary<string, int>());
+                }
+                for (int j = 1; j < data.Length; j++)
+                {
+                    if (!clothes[color].ContainsKey(data[j]))
+                    {
+                        clothes[color].Add(data[j], 0);
+                    }
+                    clothes[color][data[j]]++;
+                }
+            }
+           
+            string[] chekDress = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+
+            foreach (var curentClolor in clothes)
+            {
+                Console.WriteLine($"{curentClolor.Key} clothes:");
+                foreach (var curentClothes in curentClolor.Value)
+                {
+                    string printClothe = $"* {curentClothes.Key} - {curentClothes.Value}";
+
+                    if (curentClolor.Key == chekDress[0] && curentClothes.Key == chekDress[1])
+                    {
+                        printClothe += $" (found!)";
+                    }
+                    Console.WriteLine(printClothe);
+                }
+            }
+        }
+    }
+}
