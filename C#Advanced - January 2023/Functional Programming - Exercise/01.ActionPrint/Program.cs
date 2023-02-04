3fb4a30b6afb5126b498be704243234a73a0e641
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2023-02-04 23:41:25 +0200

    Functional Programming - Exercise
    Add problems

---
+++ C#Advanced - January 2023/Functional Programming - Exercise/01.ActionPrint/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+Action<string[]> printName = x => Console.WriteLine(string.Join(Environment.NewLine, x));
+
+string[] names = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .ToArray();
+
+printName(names);
+//PrintNames(names);
+
+//void PrintNames(string[] names)
+//{
+//    Console.WriteLine(string.Join(Environment.NewLine, names));
+//}
---
+++ C#Advanced - January 2023/Functional Programming - Exercise/02.KnightsOfHonor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Xml.Linq;
+
+Action<string[]> printNames = names =>
+{
+	foreach (var name in names)
+	{
+		Console.WriteLine($"Sir {name}");
+	}
+};
+
+string[] names = Console.ReadLine()
+	.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+	.ToArray();
+
+printNames(names);
+
+//PrintNames(names);
+
+//void PrintNames(string[] names)
+//{
+//    foreach(var name in names)
+//	{
+//        Console.WriteLine($"Sir {name}");
+//    }
+//}
