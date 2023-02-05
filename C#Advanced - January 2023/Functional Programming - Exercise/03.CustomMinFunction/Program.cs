061f6a019922efc36bc9e8294164092d32550a9a
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2023-02-05 22:59:38 +0200

    Functional Programming - Exercise
    Add problems

---
+++ C#Advanced - January 2023/Functional Programming - Exercise/03.CustomMinFunction/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+Func<int[], int> smallestNumber = n =>
+{
+    int smallesNum = int.MaxValue;
+
+    foreach (var num in n)
+    {
+        if (num < smallesNum)
+        {
+            smallesNum = num;
+        }
+
+    }
+
+    return smallesNum;
+};
+
+int[] numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+Console.WriteLine(smallestNumber(numbers));
---
+++ C#Advanced - January 2023/Functional Programming - Exercise/04.FindEvensOrOdds/Program.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+int[] range = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+string command = Console.ReadLine();
+
+Func<int, int, List<int>> generiteRange = (start, end) =>
+{
+    List<int> range = new();
+
+    for (int i = start; i <= end; i++)
+    {
+        range.Add(i);
+    }
+
+    return range;
+};
+
+List<int> numbers = generiteRange(range[0], range[1]);
+
+Predicate<int> match;
+
+if (command == "odd")
+{
+    match = n => n % 2 != 0;
+}
+else
+{
+    match = n => n % 2 == 0;
+}
+
+List<int> result = new List<int>();
+
+foreach (var num in numbers)
+{
+    if (match(num))
+    {
+        result.Add(num);
+    }
+}
+
+//Predicate<int> matchodd = n => n % 2 != 0;
+//Predicate<int> matcheven = n => n % 2 == 0;
+
+//List<int> result = new List<int>();
+
+//foreach (var num in numbers)
+//{
+//    if (command == "odd")
+//    {
+//        if (matchodd(num))
+//        {
+//            result.Add(num);
+//        }
+//    }
+//    else if (command == "even")
+//    {
+//        if (matcheven(num))
+//        {
+//            result.Add(num);
+//        }
+//    }
+//}
+
+Console.WriteLine(string.Join(" ", result));
