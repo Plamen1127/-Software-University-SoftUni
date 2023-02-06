86b07712c364a492346a64083de1bd4373e8a8ca
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2023-02-06 22:41:59 +0200

    Functional Programming - Exercise
    Add problems

---
+++ C#Advanced - January 2023/Functional Programming - Exercise/05.AppliedArithmetics/Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+Action<List<int>> printNumbers = n => Console.WriteLine(string.Join(" ", n));
+
+Func<List<int>, string, List<int>> calulatenumbers = (numbers, command) =>
+{
+    List<int> result = new List<int>();
+
+    foreach (var num in numbers)
+    {
+        switch (command)
+        {
+            case "add":
+                result.Add(num + 1);
+                break;
+            case "multiply":
+                result.Add(num * 2);
+                break;
+            case "subtract":
+                result.Add(num - 1);
+                break;
+        }
+    }
+
+    return result;
+
+};
+
+List<int> numbres = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+string command = Console.ReadLine();
+
+while (command != "end")
+{
+    if (command == "print")
+    {
+        printNumbers(numbres);
+    }
+    else
+    {
+        numbres = calulatenumbers(numbres, command);
+    }
+
+    command = Console.ReadLine();
+
+}
+
---
+++ C#Advanced - January 2023/Functional Programming - Exercise/06.ReverseAndExclude/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+Func<List<int>, List<int>> reverseNumbers = n =>
+{
+    List<int> result = new List<int>();
+
+    for (int i = n.Count - 1; i >= 0; i--)
+    {
+        result.Add(n[i]);
+    }
+
+    return result;
+};
+
+
+Func<List<int>, Predicate<int>, List<int>> excludeDivisible = (numbers, match) =>
+{
+    List<int> result = new List<int>();
+
+    foreach (var num in numbers)
+    {
+        if (match(num))
+        {
+            result.Add(num);
+        }
+    }
+
+    return result;
+};
+
+List<int> numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+int divisible = int.Parse(Console.ReadLine());
+
+Predicate<int> match = n => n % divisible != 0;
+Action<List<int>> printNumbers = n => Console.WriteLine(string.Join(" ", numbers));
+
+numbers = reverseNumbers(numbers);
+numbers = excludeDivisible(numbers, match);
+printNumbers(numbers);
