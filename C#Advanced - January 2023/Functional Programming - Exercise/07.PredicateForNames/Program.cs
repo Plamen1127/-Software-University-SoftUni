fd02f8bc51354ddb4575078accbcde58783de478
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2023-02-07 21:43:32 +0200

    Functional Programming - Exercise
    Add problems

---
+++ C#Advanced - January 2023/Functional Programming - Exercise/07.PredicateForNames/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+Action<List<string>, Predicate<string>> printFilterNames = (names, match) =>
+{
+    foreach (var name in names)
+    {
+        if (match(name))
+        {
+            Console.WriteLine(name);
+        }
+    }
+};
+int n = int.Parse(Console.ReadLine());
+
+List<string> names = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .ToList();
+
+Predicate<string> match = name => name.Length <= n;
+printFilterNames(names, match);
