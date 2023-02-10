201a4c9de4ced0950bf8691f3712ce9c54836ac3
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2023-02-10 23:56:32 +0200

    Defining Classes - Lab
    Add problems

---
+++ C#Advanced - January 2023/Defining Classes - Lab/01.Car/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+         
+        public string Make { get { return make; } set { make= value; } }
+       //public string Make { get; set; }
+
+        public string odel { get { return model; } set { model= value; } }
+        //public string Model { get; set; }
+
+        public int Year { get { return year; } set { year= value; } }
+        //public int Year { get; set; }
+
+
+
+
+
+
+
+    }
+}
---
+++ C#Advanced - January 2023/Defining Classes - Lab/02.CarExtension/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using _02.CarExtension;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car = new Car();
+
+            car.Make = "Mercedes";
+            car.Model = "C220";
+            car.Year = 2004;
+            car.FuelQuantity = 200;
+            car.FuelConsumption = 20;
+            car.Drive(10);
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
