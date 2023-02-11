7515d90a891420e440579f5f3f2804e6e5b9a49c
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2023-02-11 22:12:25 +0200

    Defining Classes - Lab
    Add problems

---
+++ C#Advanced - January 2023/Defining Classes - Lab/03.CarConstructors/Program.cs
@@ -0,0 +1,15 @@
+﻿using CarManufacturer;
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car = new Car();
+           
+           
+            
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
