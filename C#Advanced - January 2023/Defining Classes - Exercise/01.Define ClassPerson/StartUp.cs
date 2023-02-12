53379883ecd656c10556e1b5dcd8783149f8c9b8
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2023-02-12 20:52:59 +0200

    Defining Classes - Exercise
    Add problems

---
+++ C#Advanced - January 2023/Defining Classes - Exercise/01.Define ClassPerson/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using DefiningClasses;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Person person = new();
+
+            person.Name = "Plamen";
+            person.Age = 37;
+
+            Console.WriteLine($"My name i {person.Name} and I'm {person.Age} years old.");
+        }
+    }
+}
---
+++ C#Advanced - January 2023/Defining Classes - Exercise/02.CreatingConstructors/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using DefiningClasses;
+using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Person person = new();
+            Person person1 = new();
+
+           person1.Name="Plamen";
+
+            Console.WriteLine($"My name i {person.Name} and I'm {person.Age} years old.");
+            Console.WriteLine($"My name i {person1.Name} and I'm {person1.Age} years old.");
+        }
+    }
+}
