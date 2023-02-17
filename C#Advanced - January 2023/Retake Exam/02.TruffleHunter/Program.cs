363b7f0cffdc4d2091ac33bcae4a7b4a7d837b81
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2023-02-17 23:26:03 +0200

    Retake Exam

---
+++ C#Advanced - January 2023/Retake Exam/02.TruffleHunter/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Drawing;
+
+namespace _02.TruffleHunter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeMatrix = int.Parse(Console.ReadLine());
+
+            string[,] matrix = new string[sizeMatrix, sizeMatrix];
+
+            for (int row = 0; row < sizeMatrix; row++)
+            {
+                string[] data = Console.ReadLine().Split();
+
+                for (int col = 0; col < sizeMatrix; col++)
+                {
+                    matrix[row, col] = data[col];
+                }
+            }
+
+            int qtyBlackTruffle = 0;
+            int qtySummerTruffle = 0;
+            int qtyWhiteTruffle = 0;
+            int qtyEatWildBoarTruffel = 0;
+
+            string input = Console.ReadLine();
+
+            while (input != "Stop the hunt")
+            {
+                string[] inputArray = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string command = inputArray[0];
+                int row = int.Parse(inputArray[1]);
+                int col = int.Parse(inputArray[2]);
+
+                if (command == "Collect")
+                {
+
+                    if (matrix[row, col] == "B")
+                    {
+                        qtyBlackTruffle++;
+
+                    }
+                    else if (matrix[row, col] == "S")
+                    {
+                        qtySummerTruffle++;
+                    }
+                    else if (matrix[row, col] == "W")
+                    {
+                        qtyWhiteTruffle++;
+                    }
+                    if (matrix[row, col] != "-")
+                    {
+                        matrix[row, col] = "-";
+                    }
+                }
+
+                else if (command == "Wild_Boar")
+                {
+                    string direction = inputArray[3];
+
+                    if (direction == "up")
+                    {
+                        for (int r = row; r >= 0; r -= 2)
+                        {
+                            if (matrix[r, col] == "B" || matrix[r, col] == "S" || matrix[r, col] == "W")
+                            {
+                                matrix[r, col] = "-";
+                                qtyEatWildBoarTruffel++;
+                            }
+                        }
+
+                    }
+                    else if (direction == "down")
+                    {
+                        for (int r = row; r < sizeMatrix; r += 2)
+                        {
+                            if (matrix[r, col] == "B" || matrix[r, col] == "S" || matrix[r, col] == "W")
+                            {
+                                matrix[r, col] = "-";
+                                qtyEatWildBoarTruffel++;
+                            }
+                        }
+                    }
+                    else if (direction == "left")
+                    {
+                        for (int c = col; c >= 0; c -= 2)
+                        {
+                            if (matrix[row, c] == "B" || matrix[row, c] == "S" || matrix[row, c] == "W")
+                            {
+                                matrix[row, c] = "-";
+                                qtyEatWildBoarTruffel++;
+                            }
+                        }
+                    }
+                    else if (direction == "right")
+                    {
+                        for (int c = col; c < sizeMatrix; c += 2)
+                        {
+                            if (matrix[row, c] == "B" || matrix[row, c] == "S" || matrix[row, c] == "W")
+                            {
+                                matrix[row, c] = "-";
+                                qtyEatWildBoarTruffel++;
+                            }
+                        }
+                    }
+                }
+
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Peter manages to harvest {qtyBlackTruffle} black, {qtySummerTruffle} summer, and {qtyWhiteTruffle} white truffles.");
+            Console.WriteLine($"The wild boar has eaten {qtyEatWildBoarTruffel} truffles.");
+            PrintMatrix<string>(matrix);
+
+        }
+        static void PrintMatrix<T>(T[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"{matrix[row, col]} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C#Advanced - January 2023/Retake Exam/3.StockMarket/Investor.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StockMarket
+{
+    public class Investor
+    {
+        public Investor(string fullName, string emailAddress, decimal moneyToInvest, string brokerName)
+        {
+            FullName = fullName;
+            EmailAddress = emailAddress;
+            MoneyToInvest = moneyToInvest;
+            BrokerName = brokerName;
+            Portfolio = new List<Stock>();
+        }
+
+
+        public string FullName { get; set; }
+        public string EmailAddress { get; set; }
+        public decimal MoneyToInvest { get; set; }
+        public string BrokerName { get; set; }
+        public List<Stock> Portfolio { get; set; }
+        public int Count  {get { return Portfolio.Count; }}
+
+        public void BuyStock( Stock stock)
+        {
+            if (stock.MarketCapitalization>10000  && MoneyToInvest>=stock.PricePerShare)
+            {
+                Portfolio.Add(stock);
+                MoneyToInvest -= stock.PricePerShare;
+            }
+        }
+
+        public string SellStock(string campanyName, decimal sellPrice)
+        {
+            Stock sellStock = Portfolio.FirstOrDefault(s => s.CompanyName == campanyName);
+            if (sellStock ==null)
+            {
+                return $"{campanyName} does not exist.";
+            }
+            else if (sellPrice< sellStock.PricePerShare)
+            {
+                return $"Cannot sell {campanyName}.";
+            }
+            else
+            {
+                Portfolio.Remove(sellStock);
+                MoneyToInvest += sellStock.MarketCapitalization;
+                return $"{campanyName} was sold.";
+            }
+        }
+
+        public Stock FindStock( string campanyName)
+        {
+            Stock findStock = Portfolio.FirstOrDefault(s => s.CompanyName == campanyName);
+            if (findStock==null)
+            {
+                return null;
+            }
+            else
+            {
+                return findStock;
+            }
+        }
+
+        public Stock FindBiggestCompany()
+        {
+
+            if (Portfolio.Count==0)
+            {
+                return null;
+            }
+            else
+            {
+                Stock richStock = null ;
+                decimal biggestMoney = 0;
+                foreach (var stoc in Portfolio)
+                {
+                    if (stoc.MarketCapitalization > biggestMoney)
+                    {
+                        richStock = stoc;
+                        biggestMoney = stoc.MarketCapitalization;
+                    }
+                }
+                return richStock;
+            }
+        }
+
+        public  string InvestorInformation()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"The investor {FullName} with a broker {BrokerName} has stocks:");
+            foreach (var stock in Portfolio)
+            {
+                sb.AppendLine(stock.ToString());
+            }
+
+            return sb.ToString().Trim() ;
+        }
+
+    }
+}
