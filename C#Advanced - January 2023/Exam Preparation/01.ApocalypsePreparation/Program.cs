1835901fe7924985f958e71974464e46328d2866
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2023-02-21 23:46:41 +0200

    Add problems

---
+++ C#Advanced - January 2023/Exam Preparation/01.ApocalypsePreparation/Program.cs
@@ -0,0 +1,88 @@
+﻿
+
+
+using System.Collections.Specialized;
+using System.Linq;
+
+int[] input1 = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+int[] input2 = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+Dictionary<string, int> elements = new Dictionary<string, int>();
+elements.Add("Patch", 0); //30
+elements.Add("Bandage", 0); //40
+elements.Add("MedKit", 0); //100
+
+
+
+Queue<int> textiles = new Queue<int>(input1);
+Stack<int> medicaments = new Stack<int>(input2);
+
+
+while (textiles.Count > 0 && medicaments.Count > 0)
+{
+	int firstdecue = textiles.Dequeue();
+	int lastStack = medicaments.Pop();
+	int sum = firstdecue + lastStack;
+
+	if (sum == 30)
+	{
+		elements["Patch"]++;
+	}
+	else if (sum == 40)
+	{
+		elements["Bandage"]++;
+	}
+	else if (sum == 100)
+	{
+		elements["MedKit"]++;
+	}
+	else if (sum > 100)
+	{
+		elements["MedKit"]++;
+		int left = sum - 100;
+		int takeNex = medicaments.Pop();
+		medicaments.Push(left+takeNex);
+	}
+	else
+	{
+		medicaments.Push(lastStack + 10);
+	}
+}
+
+	if (textiles.Count==0 && medicaments.Count > 0)
+	{
+		Console.WriteLine("Textiles are empty.");
+	}
+	else if (medicaments.Count==0 && textiles.Count> 0)
+	{
+		Console.WriteLine("Medicaments are empty.");
+
+    }
+	else if (medicaments.Count == 0 && textiles.Count== 0)
+	{
+		Console.WriteLine("Textiles and medicaments are both empty.");
+	}
+
+Dictionary<string, int> sort = elements.OrderByDescending(e => e.Value).ThenBy(e => e.Key).ToDictionary(x => x.Key, x => x.Value);
+
+
+foreach (var element in sort)
+{
+	if (element.Value>0)
+	{
+        Console.WriteLine($"{element.Key} - {element.Value}");
+    }
+	
+}
+
+
+if (medicaments.Count > 0 )
+{
+    Console.WriteLine($"Medicaments left: {string.Join(", ", medicaments)}");
+
+}
+else if (textiles.Count > 0)
+{
+    Console.WriteLine($"Textiles left: {string.Join(", ", textiles)}");
+}
+
---
+++ C#Advanced - January 2023/Exam Preparation/02.BlindMan'sBuff/Program.cs
@@ -0,0 +1,128 @@
+﻿int[] dataSize = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+int sizeRow = dataSize[0];
+int sizeCol = dataSize[1];
+
+string[,] matrix = new string[sizeRow, sizeCol];
+int curentRow = 0;
+int curenCol = 0;
+
+for (int row = 0; row < sizeRow; row++)
+{
+    string[] data = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    for (int col = 0; col < sizeCol; col++)
+    {
+        matrix[row, col] = data[col];
+
+        if (matrix[row, col] == "B")
+        {
+            curentRow = row;
+            curenCol = col;
+        }
+    }
+}
+string command = Console.ReadLine();
+int countPozishan = 0;
+int taching = 0;
+
+while (command != "Finish")
+{
+    if (taching == 3)
+    {
+        break;
+       
+    }
+    if (command == "up")
+    {
+        curentRow--;
+        countPozishan++;
+        if (curentRow < 0 || matrix[curentRow, curenCol] == "O")
+        {
+            curentRow++;
+            countPozishan--;
+        }
+        else if (matrix[curentRow, curenCol] == "P")
+        {
+            matrix[curentRow, curenCol] = "-";
+            taching++;
+
+        }
+
+    }
+    else if (command == "down")
+    {
+        curentRow++;
+        countPozishan++;
+        if (curentRow > sizeRow - 1 || matrix[curentRow, curenCol] == "O")
+        {
+            curentRow--;
+            countPozishan--;
+
+        }
+        else if (matrix[curentRow, curenCol] == "P")
+        {
+            matrix[curentRow, curenCol] = "-";
+            taching++;
+
+        }
+
+    }
+    else if (command == "right")
+    {
+        curenCol++;
+        countPozishan++;
+        if (curenCol > sizeCol - 1 || matrix[curentRow, curenCol] == "O")
+        {
+            curenCol--;
+            countPozishan--;
+        }
+        else if (matrix[curentRow, curenCol] == "P")
+        {
+            matrix[curentRow, curenCol] = "-";
+            taching++;
+
+        }
+
+
+    }
+    else if (command == "left")
+    {
+
+        curenCol--;
+        countPozishan++;
+        if (curenCol < 0 || matrix[curentRow, curenCol] == "O")
+        {
+            curenCol++;
+            countPozishan--;
+        }
+        else if (matrix[curentRow, curenCol] == "P")
+        {
+            matrix[curentRow, curenCol] = "-";
+            taching++;
+
+        }
+    }
+
+    command = Console.ReadLine();
+}
+
+Console.WriteLine("Game over!");
+Console.WriteLine($"Touched opponents: {taching} Moves made: {countPozishan}");
+
+
+
+
+static void PrintMatrix<T>(T[,] matrix)
+{
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        for (int col = 0; col < matrix.GetLength(1); col++)
+        {
+            Console.Write($"{matrix[row, col]} ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
