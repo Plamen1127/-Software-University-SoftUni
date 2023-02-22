0da86f2f3198e675959bb2d3e50287411b119e8d
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2023-02-22 23:17:11 +0200

    Exam Preparation
    Add problem

---
+++ C#Advanced - January 2023/Exam Preparation/01.Energy Drinks/Program.cs
@@ -0,0 +1,49 @@
+﻿
+
+
+int[] dataCoffeines = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+int[] dataDrinks = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+Stack<int> coffeine = new Stack<int>(dataCoffeines);
+ Queue<int> drink = new Queue<int>(dataDrinks);
+
+int curentCoffeine = 0;
+
+while (coffeine.Count>0 && drink.Count>0)
+{
+    int lastCoffeine = coffeine.Pop();
+    int firstDrink = drink.Dequeue();
+
+    int multiplayCoffeineAndDrink = lastCoffeine * firstDrink;
+
+    if (multiplayCoffeineAndDrink+curentCoffeine <= 300)
+    {
+        curentCoffeine += multiplayCoffeineAndDrink;
+    }
+
+    else
+    {
+        if (curentCoffeine<30)
+        {
+            curentCoffeine = 0;
+        }
+        else
+        {
+            curentCoffeine -= 30;
+        }
+       
+        drink.Enqueue(firstDrink);
+    }
+}
+
+if (drink.Count>0)
+{
+    Console.WriteLine($"Drinks left: {string.Join(", ", drink)}");
+}
+else
+{
+    Console.WriteLine($"At least Stamat wasn't exceeding the maximum caffeine.");
+}
+
+Console.WriteLine($"Stamat is going to sleep with {curentCoffeine} mg caffeine.");
+
