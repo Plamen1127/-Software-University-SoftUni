351c81771d3cd590b17f62fca89853768f7a44cd
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2023-03-01 20:15:33 +0200

    Add problem

---
+++ C#Advanced - January 2023/Exam Preparation/3.Zoo/Animal.cs
@@ -0,0 +1,24 @@
+﻿namespace Zoo
+{
+    public class Animal
+    {
+        public Animal(string species, string diet, double weigth, double length)
+        {
+            Species = species;
+            Diet = diet;
+            Weigth = weigth;
+            Length = length;
+        }
+
+        public string Species { get; set; }
+        public string Diet { get; set; }
+        public double Weigth { get; set; }
+        public double Length { get; set; }
+
+        public override string ToString()
+        {
+
+            return $"The {Species} is a {Diet} and weighs {Weigth} kg.";
+        }
+    }
+}
---
+++ C#Advanced - January 2023/Exam Preparation/3.Zoo/Zoo.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace Zoo
+{
+    public class Zoo
+    {
+
+        
+
+        public Zoo(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            Animals = new List<Animal>();
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public List<Animal> Animals { get; }
+        
+
+        public string AddAnimal( Animal animal)
+        {
+            if(string.IsNullOrWhiteSpace(animal.Species))
+            {
+                return "Invalid animal species.";
+            }
+            else if (animal.Diet != "herbivore" && animal.Diet != "carnivore")
+            {
+                return "Invalid animal diet.";
+            }
+            else if (Capacity==Animals.Count)
+            {
+                return "The zoo is full.";
+            }
+            else
+            {
+                Animals.Add(animal);
+                return $"Successfully added {animal.Species} to the zoo.";
+            }
+
+        }
+
+        public int RemoveAnimals( string species)
+             => Animals.RemoveAll(s => s.Species == species);
+
+        public List<Animal> GetAnimalsByDiet(string diet)
+        => Animals.FindAll(s => s.Diet == diet);
+
+        public Animal GetAnimalByWeight(double weight)
+            => Animals.FirstOrDefault(a => a.Weigth == weight);
+
+        public string GetAnimalCountByLength( double minimumLenght, double maximumLenght)
+        {
+            List<Animal> sortedAnimal = new List<Animal>();
+            if (Animals.Count==0)
+            {
+                return $"There are {0} animals with a length between {minimumLenght} and {maximumLenght} meters.";
+            }
+            else
+            {
+                foreach (var animal in Animals)
+                {
+                    if (animal.Length >= minimumLenght && animal.Length <= maximumLenght)
+                    {
+                        sortedAnimal.Add(animal);
+                    }
+                }
+            }
+           
+
+      
+
+            return $"There are {sortedAnimal.Count} animals with a length between {minimumLenght} and {maximumLenght} meters.";
+        }
+    }
+}
