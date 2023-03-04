b9984144a52050314450b5831cc919fb91b4a89b
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2023-03-04 23:00:11 +0200

    Add problems

---
+++ C# OPP - February 2023/Inheritance - Exercise/03.PayersAndMonsters/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            SoulMaster am = new SoulMaster("plamen", 5);
+            Console.WriteLine(am);
+        }
+    }
+}
