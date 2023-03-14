5e064c24d37e9794c489f597a1a2d5c6da40c78b
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2023-03-14 22:47:58 +0200

    Add Problems

---
+++ C# OPP - February 2023/Encapsulation - Exercise/03.ShoppingSpree/ExceptionMessages.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace ShoppingSpree.Models
+{
+    public class ExceptionMessages
+    {
+        public const string NameExceptionMessage = "Name cannot be empty";
+        public const string MoneyExeptionMessage = "Money cannot be negative";
+    }
+}
---
+++ C# OPP - February 2023/Encapsulation - Exercise/04.PizzaCalories/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace PizzaCallories.Models
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                string[] pizzaData = Console.ReadLine().Split();
+                string[] doughData = Console.ReadLine().Split();
+
+                Dough dough = new(doughData[1], doughData[2], double.Parse(doughData[3]));
+
+                string pizzaName = pizzaData[1];
+
+                Pizza pizza = new(pizzaName, dough);
+                string command;
+                while ((command=Console.ReadLine())!="END")
+                {
+                    string[] toppingInfo = command.Split();
+
+                    Topping topping = new(toppingInfo[1], double.Parse(toppingInfo[2]));
+
+                    pizza.AddTopping(topping);
+
+                }
+
+                Console.WriteLine(pizza.ToString());
+  
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }       
+           
+        }
+    }
+}
