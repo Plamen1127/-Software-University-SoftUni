7ee94e794de7bea5a1efff0d5e7a4cb2a6eddaa6
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2023-03-28 22:40:11 +0300

    Add problem

---
+++ C# OPP - February 2023/Polymorphism - Lab/03.Shapes/Rectangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+      
+
+        public Rectangle(double height, double width)
+        {
+            Height = height;
+            Widht = width;
+        }
+
+        public double Height { get; private set; }
+        public double Widht { get; private set; }
+
+        public override double CalculateArea()
+        {
+            return Widht * Height;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return Widht * 2 + Height * 2;
+        }
+    
+
+        public override string Draw()
+        {
+            return base.Draw() + $" {this.GetType().Name}";
+        }
+    }
+}
---
+++ C# OPP - February 2023/Polymorphism - Lab/03.Shapes/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Shape circule = new Circle(2.5);
+            Shape rectangel = new Rectangle(2.5, 2.5);
+
+            Console.WriteLine(circule.CalculateArea());
+            Console.WriteLine(circule.Draw());
+        }
+    }
+}
