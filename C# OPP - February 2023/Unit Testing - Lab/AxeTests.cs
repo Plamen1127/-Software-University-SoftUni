fa06ec344a730f4c2a987d232ad9f0c6c1b3f1a0
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2024-03-27 18:01:32 +0200

    Unit Testing - Lab

---
+++ C# OPP - February 2023/Unit Testing - Lab/AxeTests.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework;
+using System;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class AxeTests
+    {
+        [Test]
+        public void Test_Attack_Looses_Weapon_Durability()
+        {
+            Axe axe = new Axe(10, 20);
+
+            axe.Attack(new Dummy(10, 100));
+
+            Assert.AreEqual(axe.DurabilityPoints, 19);
+        }
+
+        [Test]
+        public void Test_Attack_With_Zero_Durability_Throws_Error()
+        {
+            Axe axe = new Axe(10, 1);
+
+            axe.Attack(new Dummy(100, 100));
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                axe.Attack(new Dummy(100, 100));
+            });
+        }
+    }
+}
---
+++ C# OPP - February 2023/Unit Testing - Lab/DummyTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        [Test]
+        public void Chec_Is_Dummy_Loses_Healt_Corectly()
+        {
+            Dummy dummy = new Dummy(10, 10);
+
+            dummy.TakeAttack(5);
+
+            Assert.AreEqual(5, dummy.Health);
+        }
+
+        [Test]  
+        public void Dead_Dummy_Shuld_Throw_Exception_When_Attacked()
+        {
+            Dummy dummy= new Dummy(0, 10);
+
+            Assert.Throws<InvalidOperationException>(() => dummy.TakeAttack(5), "Dummy accept attach when dead");
+        }
+
+        [Test]
+        public void Dummy_Give_Exspirience_When_Dies()
+        {
+            Dummy dummy= new Dummy(0, 10);
+
+            int giveExspiriance = dummy.GiveExperience();
+
+            Assert.AreEqual(10,giveExspiriance);
+        }
+
+        [Test]
+
+    public void Dummy_Cant_Give_Exspirience_When_Alive()
+        {
+            Dummy dummy = new Dummy(10,10);
+
+            Assert.Throws<InvalidOperationException>( () => dummy.GiveExperience(), "Alive Dummy cant give exspiriance");
+        }
+    }
+}
