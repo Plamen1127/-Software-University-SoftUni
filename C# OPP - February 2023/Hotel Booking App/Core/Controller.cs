246823cb1eb9f574ce219615792c8ddb6e51faf6
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2024-04-17 01:34:48 +0300

    Hotel Booking App

---
+++ C# OPP - February 2023/Hotel Booking App/Core/Controller.cs
@@ -0,0 +1,195 @@
+﻿using BookingApp.Core.Contracts;
+using BookingApp.Models.Bookings;
+using BookingApp.Models.Bookings.Contracts;
+using BookingApp.Models.Hotels;
+using BookingApp.Models.Hotels.Contacts;
+using BookingApp.Models.Rooms;
+using BookingApp.Models.Rooms.Contracts;
+using BookingApp.Repositories;
+using BookingApp.Repositories.Contracts;
+using BookingApp.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Core
+
+{
+    public class Controller : IController
+    {
+        private readonly IRepository<IHotel> hotels;
+        private string[] roomType = new string[] { typeof(Apartment).Name, typeof(DoubleBed).Name, typeof(Studio).Name };
+
+        public Controller()
+        {
+            hotels = new HotelRepository();
+        }
+        public string AddHotel(string hotelName, int category)
+        {
+            IHotel newHotel = hotels.Select(hotelName);
+
+            if (newHotel != null)
+            {
+                return string.Format(OutputMessages.HotelAlreadyRegistered, hotelName);
+            }
+
+            newHotel = new Hotel(hotelName, category);
+
+            this.hotels.AddNew(newHotel);
+
+            return string.Format(OutputMessages.HotelSuccessfullyRegistered, category, hotelName);
+        }
+
+        public string UploadRoomTypes(string hotelName, string roomTypeName)
+        {
+            IHotel hotel = hotels.Select(hotelName);
+
+            if (hotel == null)
+            {
+                return string.Format(OutputMessages.HotelNameInvalid, hotelName);
+            }
+
+            IRoom room = hotel.Rooms.Select(roomTypeName);
+
+            if (room != null)
+            {
+                return string.Format(OutputMessages.RoomTypeAlreadyCreated);
+            }
+
+            if (!roomType.Contains(roomTypeName))
+            {
+                throw new ArgumentException(ExceptionMessages.RoomTypeIncorrect);
+            }
+
+            room = CreateRoom(roomTypeName);
+
+            hotel.Rooms.AddNew(room);
+
+            return string.Format(OutputMessages.RoomTypeAdded, roomTypeName, hotelName);
+        }
+
+        public string SetRoomPrices(string hotelName, string roomTypeName, double price)
+        {
+            IHotel hotel = hotels.Select(hotelName);
+
+            if (hotel == null)
+            {
+                return string.Format(OutputMessages.HotelNameInvalid, hotelName);
+            }
+
+            if (!roomType.Contains(roomTypeName))
+            {
+                throw new ArgumentException(ExceptionMessages.RoomTypeIncorrect);
+            }
+
+            IRoom room = hotel.Rooms.Select(roomTypeName);
+
+            if (room == null)
+            {
+                return string.Format(OutputMessages.RoomTypeNotCreated);
+            }
+            if (room.PricePerNight > 0)
+            {
+                throw new InvalidOperationException(ExceptionMessages.PriceAlreadySet);
+            }
+
+            room.SetPrice(price);
+
+            return string.Format(OutputMessages.PriceSetSuccessfully, roomTypeName, hotelName);
+        }
+
+        public string BookAvailableRoom(int adults, int children, int duration, int category)
+        {
+            IHotel hotel = hotels.All().FirstOrDefault(h => h.Category == category);
+            if (hotel == null)
+            {
+                return string.Format(OutputMessages.CategoryInvalid, category);
+            }
+
+            IOrderedEnumerable<IHotel> sortedHotel = hotels.All()
+                .Where(h => h.Category == category)
+                .OrderBy(h => h.FullName);
+
+            foreach (var currentHotel in sortedHotel)
+            {
+                IRoom room = currentHotel.Rooms
+                    .All()
+                    .Where(r => r.PricePerNight > 0)
+                    .OrderBy(r => r.BedCapacity)
+                    .FirstOrDefault(r => r.BedCapacity >= adults + children);
+
+                if (room != null)
+                {
+                    int bookingNumber = hotel.Bookings.All().Count() + 1;
+
+                    IBooking booking = new Booking(room, duration, adults, children, bookingNumber);
+
+                    hotel.Bookings.AddNew(booking);
+
+                    return string.Format(OutputMessages.BookingSuccessful, bookingNumber, currentHotel.FullName);
+                }
+
+            }
+
+            return string.Format(OutputMessages.RoomNotAppropriate);
+
+        }
+
+        public string HotelReport(string hotelName)
+        {
+            IHotel hotel = hotels.Select(hotelName);
+
+            if (hotel == null)
+            {
+                return string.Format(OutputMessages.HotelNameInvalid, hotelName);
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Hotel name: {hotel.FullName}");
+            sb.AppendLine($"--{hotel.Category} star hotel");
+            sb.AppendLine($"--Turnover: {hotel.Turnover:F2} $");
+            sb.AppendLine($"--Bookings:");
+            sb.AppendLine();
+
+            if (hotel.Bookings.All().Count == 0)
+            {
+                sb.AppendLine("none");
+            }
+            else
+            {
+                foreach (var booking in hotel.Bookings.All())
+                {
+                    sb.AppendLine(booking.BookingSummary());
+                    sb.AppendLine();
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+
+        }
+
+
+        private IRoom CreateRoom(string roomTypeName)
+        {
+            IRoom room;
+            if (roomTypeName == "Apartment")
+            {
+                room = new Apartment();
+            }
+            else if (roomTypeName == "DoubleBed")
+            {
+                room = new DoubleBed();
+            }
+            else
+            {
+                room = new Studio();
+            }
+
+            return room;
+        }
+
+    }
+}
