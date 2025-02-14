5d35e6f4b86f76f69a4f895edcd7cb8f7780dd46
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2025-02-14 16:50:47 +0200

    Entity Framework Introduction
    Problem 2 and 3

---
+++ Entity Framework Core - February 2025/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using SoftUni.Data;
+using System.Text;
+
+namespace SoftUni
+{
+
+
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var context = new SoftUniContext();
+            Console.WriteLine(GetEmployeesFullInformation(context));
+
+        }
+
+        // problem 1
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            // option 1 
+            //return string.Join(Environment.NewLine, context.Employees
+            //  .Select(e => $"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}")
+            //   .ToList());
+
+
+            // option 2
+            var employees = context.Employees
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.MiddleName,
+                    e.JobTitle,
+                    e.Salary
+                })
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}");
+            }
+
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
