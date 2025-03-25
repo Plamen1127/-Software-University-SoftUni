84dd4b5ae1e4d9dbbfe090105b48b0f7d846308f
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2025-03-25 23:46:09 +0200

    LINQ

---
+++ Entity Framework Core - February 2025/LINQ/MusicHub/Data/Models/Writer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicHub.Data.Models
+{
+    public class Writer
+    {
+        public Writer()
+        {
+            Songs = new HashSet<Song>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Name  { get; set; }
+
+        public string? Pseudonym  { get; set; }
+
+        
+        public virtual ICollection<Song> Songs { get; set; }
+    }
+}
